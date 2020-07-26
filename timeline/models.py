2250e3baf8399fe03dfe47d0a827c3f7d0de69ec
Rishabh Prajapati <rishabhp@dosepack.com>
2020-07-26 15:16:42 +0530

    Added timeline app and its models.

---
+++ timeline/models.py
@@ -0,0 +1,38 @@
+import datetime
+from pathlib import Path
+
+from django.db import models
+
+TEXT_SHORT = 25
+SMALL_TEXT = 256
+LARGE_TEXT = 1500
+
+
+# Create your models here.
+class Moment(models.Model):
+    title = models.CharField(max_length=SMALL_TEXT)
+    description = models.CharField(max_length=LARGE_TEXT, blank=True)
+    timestamp = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.title
+
+
+def moment_gallery_path(instance, filename):
+    return Path('timeline') / str(instance.moment) / filename
+
+
+class Media(models.Model):
+    moment = models.ForeignKey(Moment, related_name='gallery', on_delete=models.CASCADE)
+    file = models.FileField(upload_to=moment_gallery_path)
+    TYPE_CHOICES = (
+        ('i', 'Image'),
+        ('v', 'Video'),
+    )
+    type = models.CharField(max_length=TEXT_SHORT, choices=TYPE_CHOICES)
+
+    class Meta:
+        verbose_name_plural = 'Media'
+
+    def __str__(self):
+        return f'{self.moment}:{self.file.name}'
--- uBlog/settings.py
+++ uBlog/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'timeline',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
