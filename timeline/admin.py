703e691a08517d33e7ebe206db1f4e35b5bcef60
Rishabh Prajapati <rishabhp@dosepack.com>
2020-07-27 18:32:02 +0530

    Added tabular inline of media in moments admin.

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+
+class MediaInline(admin.TabularInline):
+    model = Media
+
+
+class MomentAdmin(admin.ModelAdmin):
+    inlines = [
+        MediaInline,
+    ]
+
+
 # Register your models here.
-admin.site.register(Moment)
+admin.site.register(Moment, MomentAdmin)
 admin.site.register(Media)
