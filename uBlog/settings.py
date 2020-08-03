58c29017fc4fc77b99f0e828d6e74db8b7b74b4d
rishabh <rishabh.prajapati0@gmail.com>
2020-08-03 20:33:47 +0530

    Added navbar on base template

--- uBlog/settings.py
+++ uBlog/settings.py
@@ -115,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
